273b6c2b3814a042ab50e6c4510cfcf3bcde9f19
Rohit Kumar <97929611+rohitnitjsr1801@users.noreply.github.com>
2023-07-28 11:47:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-const dbConnection = require('./db');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const dbConnection = require('./db')
+app.use(express.json())
 
-app.use(express.json());
+app.use('/api/cars/' , require('./routes/carsRoute'))
+app.use('/api/users/' , require('./routes/usersRoute'))
+app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
-app.use('/api/cars/', require('./routes/carsRoute'));
-app.use('/api/users/', require('./routes/usersRoute'));
-app.use('/api/bookings/', require('./routes/bookingsRoute'));
 
-const path = require('path');
+const path = require('path')
 
-if (process.env.NODE_ENV === 'production') {
-  // Set the static folder to 'client/build' for serving the built React app
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if(process.env.NODE_ENV==='production')
+{
 
-  // Serve the index.html for all other routes in production
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+
+    })
+
 }
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`));
+
+ 
+
+
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
